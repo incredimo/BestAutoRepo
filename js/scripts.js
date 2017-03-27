11a01046aa983cfc4e3e2615b495ff1b9f6ffcdc
aghil4u <aghilonline4u@gmail.com>
2017-03-27 17:23:02 +0400

    code upgrade maps

--- js/scripts.js
+++ js/scripts.js
@@ -8,33 +8,24 @@ function initMap() {
     scrollwheel:false,
   });
 
-    // location={ lat: 24.440540, lng: 54.444341 };
-  addMarker();
+ loc={ lat: 24.440540, lng: 54.444341, Name:"Branch1", Address:"abu dhabi, 2345632, opposit to dalma mall" };
+  addMarker(loc);
+
+  loc={ lat: 24.50, lng: 54.444333, Name:"Branch2", Address:"abu dhabi, 2345632, opposit to dalma mall" };
+   addMarker(loc);
 }
 
 
 // Adds a marker to the map.
-   function addMarker() {
+   function addMarker(location) {
 
 
               var contentString = '<div id="content">'+
                        '<div id="siteNotice">'+
                        '</div>'+
-                       '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+                       '<h1 id="firstHeading" class="firstHeading">'+ location.Name+'</h1>'+
                        '<div id="bodyContent">'+
-                       '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-                       'sandstone rock formation in the southern part of the '+
-                       'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-                       'south west of the nearest large town, Alice Springs; 450&#160;km '+
-                       '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-                       'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-                       'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-                       'Aboriginal people of the area. It has many springs, waterholes, '+
-                       'rock caves and ancient paintings. Uluru is listed as a World '+
-                       'Heritage Site.</p>'+
-                       '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-                       'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-                       '(last visited June 22, 2009).</p>'+
+                       '<p>'+location.Address+'</p>'+
                        '</div>'+
                        '</div>';
 
@@ -46,12 +37,15 @@ function initMap() {
 
 
             var marker = new google.maps.Marker({
-              position: {lat: 24.440540, lng: 54.444341},
-              label: "A",
+              position: {lat: location.lat , lng:location.lng},
+
               map: map
             });
 
             marker.addListener('click', function() {
+                if (infowindow) {
+       infowindow.close();
+                map.setCenter(marker.getPosition());
                 infowindow.open(map, marker);
               });
 
